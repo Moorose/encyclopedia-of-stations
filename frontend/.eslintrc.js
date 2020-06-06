2ed5d3dd7bd6bc4d31171fc47ff3740a8694aaef
Eugene Vigonny <eugene_vigonny@lineate.com>
2020-06-06 15:28:12 +0600

    EOS-5 update eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -12,15 +12,55 @@ module.exports = {
   },
   rules: {
     'padding-line-between-statements': ['error',
-      { 'blankLine': 'always', 'prev': '*', 'next': 'return' },
-      { 'blankLine': 'always', 'prev': ['const', 'let'], 'next': '*' },
-      { 'blankLine': 'always', 'prev': '*', 'next': ['const', 'let'] },
-      { 'blankLine': 'any', 'prev': ['const', 'let'], 'next': ['const', 'let'] },
+      {
+        'blankLine': 'always',
+        'prev': '*',
+        'next': 'return'
+      },
+      {
+        'blankLine': 'always',
+        'prev': ['const', 'let'],
+        'next': '*'
+      },
+      {
+        'blankLine': 'always',
+        'prev': '*',
+        'next': ['const', 'let']
+      },
+      {
+        'blankLine': 'any',
+        'prev': ['const', 'let'],
+        'next': ['const', 'let']
+      },
     ],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    indent: 'off',
+    'linebreak-style': ['error', 'unix'],
+    'import/extensions': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'max-len': ['error', { 'code': 120 }],
+    'vue/script-indent': ['error', 2, { 'baseIndent': 1 }],
+    'indent': ['error', 2],
+    'operator-linebreak': 'off',
+    'vue/no-parsing-error': 'off',
+    'vue/valid-v-if': 'off',
+    'no-param-reassign': 'off',
+    'arrow-body-style': 'off',
+    'no-shadow': ['error',
+      {
+        'builtinGlobals': false,
+        'hoist': 'functions',
+        'allow': ['getters']
+      },
+    ],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+  'overrides': [
+    {
+      'files': ['*.vue'],
+      'rules': {
+        'indent': 'off'
+      }
+    }
+  ]
 };
