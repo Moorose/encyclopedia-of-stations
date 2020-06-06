03c102c4a23cfe5c7218d20a05172befe116a42a
Eugene Vigonny <eugene_vigonny@lineate.com>
2020-06-06 15:28:58 +0600

    EOS-5 clear route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,45 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '@/views/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import('@/views/About.vue'),
-  },
-  {
-    path: '/map',
-    name: 'Map',
-    component: () => import('@/views/Map.vue'),
-  },
-  {
-    path: '/search-station',
-    name: 'SearchStation',
-    component: () => import('@/views/SearchStation.vue'),
-  },
-  {
-    path: '/add-station',
-    name: 'AddStation',
-    component: () => import('@/views/AddStation.vue'),
-  },
-  {
-    path: '/station',
-    name: 'Station',
-    component: () => import('@/views/Station.vue'),
-  },
-  {
-    path: '/profile',
-    name: 'Profile',
-    component: () => import('@/views/Profile.vue'),
-  },
+  { path: '/about', component: () => import('@/views/About.vue'), name: 'About' },
+  { path: '/map', component: () => import('@/views/Map.vue'), name: 'Map' },
+  { path: '/search', component: () => import('@/views/SearchStation.vue'), name: 'SearchStation' },
+  { path: '/station/add', component: () => import('@/views/AddStation.vue'), name: 'AddStation' },
+  { path: '/station/:id', component: () => import('@/views/Station.vue'), name: 'Station' },
+  { path: '/profile', component: () => import('@/views/Profile.vue'), name: 'Profile' },
 ];
 
 const router = new VueRouter({
