1634182565bc5c05bfe4b6548bab484246fbdc83
Eugene Vigonny <eugene_vigonny@lineate.com>
2020-06-07 19:18:17 +0600

    Change json ormconfig to js

---
+++ backend/ormconfig.js
@@ -0,0 +1,24 @@
+module.exports = {
+  'type': 'postgres',
+  'host': 'localhost',
+  'port': 5432,
+  'username': process.env.TYPEORM_USERNAME,
+  'password': process.env.TYPEORM_PASSWORD,
+  'database': 'storage',
+  'synchronize': true,
+  'logging': false,
+  'entities': [
+    'src/entity/**/*.ts'
+  ],
+  'migrations': [
+    'src/migration/**/*.ts'
+  ],
+  'subscribers': [
+    'src/subscriber/**/*.ts'
+  ],
+  'cli': {
+    'entitiesDir': 'src/entity',
+    'migrationsDir': 'src/migration',
+    'subscribersDir': 'src/subscriber'
+  }
+};
